af70f1d58b5b149cf827888fdd37a302a0ae9b8c
Tim O'Malley <tomalley24@gmail.com>
2021-06-12 14:51:53 -0500

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,13 +42,11 @@ self.addEventListener("install", function(evt) {
   
   // fetch
   self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -56,7 +54,6 @@ self.addEventListener("install", function(evt) {
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
@@ -64,9 +61,7 @@ self.addEventListener("install", function(evt) {
   
       return;
     }
-  
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+
     evt.respondWith(
       caches.match(evt.request).then(function(response) {
         return response || fetch(evt.request);
